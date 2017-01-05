d79e9b8aca9ba8de1f177441eac2e5bf4f38187a
gordonrox <gordon_roxburgh@hotmail.com>
2017-01-05 18:21:56 +0000

    Adding my hello world program.

---
+++ Hello World/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Press Any Key to Exit!");
+            Console.ReadKey();
+        }
+    }
+}
